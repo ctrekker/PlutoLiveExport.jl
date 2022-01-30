8bf702f52e910d37bfc3f91e2f3e4ae7e50df115
Connor Burns <ctrekker4@gmail.com>
2022-01-29 20:03:47 -0700

    🌥️

--- src/PlutoLiveExport.jl
+++ src/PlutoLiveExport.jl
@@ -1,5 +1,18 @@
 module PlutoLiveExport
 
-greet() = print("Hello World!")
+import Pluto
+
+export live_export
+
+
+
+
+function live_export(; format::String = "html", on::Pluto.PlutoEvent = Pluto.FileEditEvent)
+
+end
+
+function live_export(; formats::Vector{String}, kwargs...)
+
+end
 
 end # module
